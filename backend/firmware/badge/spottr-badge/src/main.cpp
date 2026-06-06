1fd19fafe81eb2751e60b4b689693e2030eb2207
BrunoGG69 <prathameshprabhakar27@gmail.com>
2026-06-06 17:13:45 +0000

    feat: start work on badge firmware which advertises via BLE

---
+++ backend/firmware/badge/spottr-badge/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <NimBLEDevice.h>
+
+#define BADGE_ID "SPOTTR_BADGE_001"
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 10
+#endif
+
+NimBLEAdvertising *adv;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  NimBLEDevice::init(BADGE_ID);
+
+  NimBLEAdvertisementData advData;
+  advData.setName(BADGE_ID);
+  advData.setManufacturerData(std::string(BADGE_ID));
+
+  adv = NimBLEDevice::getAdvertising();
+  adv->setAdvertisementData(advData);
+  adv->start();
+
+  Serial.println("Badge advertising continuously...");
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  Serial.println("Badge is running...");
+  delay(1000);
+}
